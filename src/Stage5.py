263eac3d662ae4cf009aaca180373066cd70111e
Matheus Sanchez <matheus2.sanchez@usp.br>
2020-03-04 17:31:02 -0300

    Modularization of StageScreen and Stage5 (without test)

--- src/Stage5.py
+++ src/Stage5.py
@@ -4,17 +4,16 @@ from utils import *
 from StageScreen import StageScreen
 import numpy as np
 
-class Stage2(StageScreen):
+class Stage5(StageScreen):
 	def __init__(self, master, prev_sc, main_bg):
 		super().__init__(master, prev_sc, main_bg)
 
-		self.reinforced_clicks = random.sample(self.VR5,5)
-		print(self.reinforced_clicks) # five numbers of list VR5 without replacement
-		self.reinforced_clicks = np.cumsum(self.reinforced_clicks) # accumulated sum of list VR5 without replacement
+		self.reinforced_clicks = random.sample(self.VR20,5)
+		print(self.reinforced_clicks) # five numbers of list VR20 without replacement
+		self.reinforced_clicks = np.cumsum(self.reinforced_clicks) # accumulated sum of list VR20 without replacement
 		print(self.reinforced_clicks)
 
 	def conditionalReforce(self):
-		print("This is conditionalReforce of Stage2")
 		return sum(self.game['frequency'].values()) in self.reinforced_clicks
 
 
