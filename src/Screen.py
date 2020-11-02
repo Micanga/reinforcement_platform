uploading the IRT fix formula - fixed for 6 blocks

src/Screen.py
@@ -460,37 +460,21 @@ class Screen(Game):
         print(exit_log)
 
     def averageIRT(self):
-        #blocks that have to be used to calculate mean
-        firstId = len(self.game) - self.settings['min_blocks']
-        lastId = len(self.game) 
-        
-        #getting all Id's for all answers
-        time2Answer = [] # array with all the time
-        for i in  range (firstId,lastId):
-            print("| --------- BLOCO")
-            print(self.game[i])
-            idAnswer= [j for j, val in enumerate(self.game[i]['reinforced'])] 
-
-            #getting all the times that we reinforced
-            
-            for j in  idAnswer:
-                time2Answer.append(self.game[i]['time2answer'][j])
-            
-          
-        #just calculating averageIRT 
-        averageIRT = 0
-        if(len(time2Answer) != 0):
-            for i in range(1,len(time2Answer)):
-                averageIRT += (time2Answer[i].total_seconds() - time2Answer[i-1].total_seconds())
-            averageIRT /= (len(time2Answer)-1)
-
-            print("Your IRT = ")
-            print(averageIRT)
-
-            print("IRT min = ")
-            print(self.settings['IRT_threshold'])
-            
-            return (averageIRT)    
-
-        else:
+        # verifying if the number of blocks is sufficient to calculate the averageIRT
+        if self.settings['min_blocks'] < 6:
             return 1.0
+
+        # calculating irt
+        # a. getting the mean time per block
+        irt_per_block = []
+        for i in range( len(self.game)-self.settings['min_blocks']-1, len(self.game)):
+            irt_per_block.append(np.mean(self.game[i]['time2answer']))
+
+        # b. calculating (for real)
+        R = irt_per_block[0] + irt_per_block[1] + irt_per_block[2])/3 -
+                (irt_per_block[3] + irt_per_block[4]] + irt_per_block[5])/3) 
+        R /= (np.sum(irt_per_block)/6)
+
+        # returning the result
+        return np.abs(R)
+            